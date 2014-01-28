3dc1ea54a88db2412eb8a372fa1bff4ee408994e
Mitchtlam <sagacrby@hotmail.com>
2014-01-28 10:50:27 +0100

    Toets2Panda

---
+++ coworker.php
@@ -0,0 +1,2 @@
+
+<a href="http://nl.wikipedia.org/wiki/Fiat_Panda"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRy8qoRbJ7jvhsWox4nv9gcYhvu6WyjgeUoVCj_GKwWWvCRGD3Q" /> </a>
--- link.php
+++ link.php
@@ -44,7 +44,12 @@
 					echo "<li>
 							<a href=''>pho-link</a>
 						  </li>";
-				break;				
+				break;		
+				case 'coworker':
+					echo "<li>
+							<a href='index.php?content=coworker'>panda</a>
+						  </li>";
+				break;			
 			}
 		}
 		else
